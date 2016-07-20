2f7916347772fca0748b33b61eab116640a30871
Sergei Morozov <siarhei_marozau@epam.com>
2016-07-20 18:33:18 +0300

    first task

--- Data Structures/week1/check_brackets_in_code/check_brackets.py
+++ Data Structures/week1/check_brackets_in_code/check_brackets.py
@@ -17,16 +17,35 @@ class Bracket:
         return False
 
 if __name__ == "__main__":
-    text = sys.stdin.read()
+    #text = sys.stdin.read()
 
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
-            pass
+    def bracket_check(text):
+        opening_brackets_stack = []
+        for i, next in enumerate(text):
+            if next == '(' or next == '[' or next == '{':
+                # Process opening bracket, write your code here
+                opening_brackets_stack.append(Bracket(next,i))
+            elif next == ')' or next == ']' or next == '}':
+                # Process closing bracket, write your code here
+                if not len(opening_brackets_stack):
+                    return i + 1
+                last = opening_brackets_stack.pop()
+                if not last.Match(next):
+                    return i+1
+        if len(opening_brackets_stack):
+            return opening_brackets_stack.pop().position+1
+        else:
+            return 'Success'
 
-        if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
-            pass
-
-    # Printing answer, write your code here
+    def test():
+        assert(bracket_check('}') == 1)
+        assert(bracket_check('{}[]') == 'Success')
+        assert(bracket_check('[()]') == 'Success')
+        assert(bracket_check('(())') == 'Success')
+        assert(bracket_check('{[]}()') == 'Success')
+        assert(bracket_check('{') == 1)
+        assert(bracket_check('{[}') == 3)
+        assert(bracket_check('foo(bar);') == 'Success')
+        assert(bracket_check('foo(bar[i);') == 10)
+    test()
+    #print(bracket_check(sys.stdin.read()))
