308e200b57d0349b0e547d43b96dd04415ead0f0
Sergei Morozov <siarhei_marozau@epam.com>
2016-09-21 14:39:16 +0300

    finished data structures course

--- Data Structures/week2/merging_tables/merging_tables.py
+++ Data Structures/week2/merging_tables/merging_tables.py
@@ -9,23 +9,33 @@ parent = list(range(0, n))
 ans = max(lines)
 
 def getParent(table):
-    # find parent and compress path
+    if table != parent[table]:
+        parent[table] = getParent(parent[table])
     return parent[table]
 
 def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
-
-    if realDestination == realSource:
+    global ans,lines
+    rDest, rSource = getParent(destination), getParent(source)
+    if rDest == rSource:
         return False
-
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
+    if rank[rSource] > rank[rDest]:
+        lines[rSource] += lines[rDest]
+        if lines[rSource] > ans:
+            ans = lines[rSource]
+        lines[rDest] = 0
+        parent[rDest] = rSource
+    else:
+        lines[rDest] += lines[rSource]
+        if lines[rDest] > ans:
+            ans = lines[rDest]
+        lines[rSource] = 0
+        parent[rSource] = rDest
+        if rank[rSource] == rank[rDest]:
+            rank[rDest] += 1
     return True
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
     print(ans)
-    
+
