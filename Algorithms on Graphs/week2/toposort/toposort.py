c8b79fb158e4d5d674c84e991ef59833bb100a86
Morozov-Sergei <Rilyntaar@gmail.com>
2017-09-20 11:15:23 +0300

    1st assignment week2 completed

--- Algorithms on Graphs/week2/toposort/toposort.py
+++ Algorithms on Graphs/week2/toposort/toposort.py
@@ -1,17 +1,102 @@
-#Uses python3
+#Uses python2
+
 
 import sys
+class ADJL():
+    def __init__(self, adj):
+        self.__data = {k: adj[k] for k in xrange(len(adj))}
+        self.__visited = {k:False for k in range(0, len(adj))}
+        self.__postorder = {k:0 for k in range(0, len(adj))}
+        self.__post = 0
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
+    @property
+    def max_post(self):
+        idx, val = self.postorder.items()[0]
+        for k, v in self.postorder.items():
+            if v > val:
+                idx = k
+                val = v
+        return idx, val
+
+    @property
+    def postorder(self):
+        return self.__postorder
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def visited(self):
+        return self.__visited
+
+    def get_edges(self, v):
+        return self.data[v]
+
+    def __str__(self):
+        result = ''
+        for k, v in self.data.items():
+            result += 'VERTEX:' + str(k) + ' Edges: ' + str(v)+ ' #POST: ' + str(self.postorder[k]) + '\n'
+        return result
+
+    def make_reversed(self):
+        newdata = {k:[] for k in xrange(len(self.data.keys()))}
+        for i, v in self.data.items():
+            for e in v:
+                newdata[e].append(i)
+        self.data = newdata
+
+    def mark_dfs(self):
+        self.postorder = {k:0 for k in self.data.keys()}
+        self.visited = {k:False for k in self.data.keys()}
+        for v in self.data.keys():
+            if not self.visited[v]:
+                self.dfs(v)
+                self.__post += 1
+                self.postorder[v] = self.__post
+    def dfs(self, x):
+        explored_v = [x]
+        self.visited[x] = True
+        for v in self.get_edges(x):
+            if not self.visited[v]:
+                explored_v.extend(self.dfs(v))
+        self.__post += 1
+        self.postorder[x] = self.__post
+        return explored_v
+
+    def post_remove(self, node):
+        self.postorder.pop(node)
+    def node_remove(self, node):
+        ver = self.data[node]
+        for k,v in self.data.items():
+            if node in v:
+                v.remove(node)
+                v.extend(ver)
+        self.data.pop(node)
+
+
+
+
+
+
 
 
 def toposort(adj):
-    used = [0] * len(adj)
+
     order = []
-    #write your code here
+
+    invest = ADJL(adj)
+
+
+    for i in xrange(len(adj)):
+        invest.mark_dfs()
+        idx, val = invest.max_post
+        order.append(idx)
+        invest.node_remove(idx)
+        invest.post_remove(idx)
     return order
+
+#sys.stdin = file('../tests/a')
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -24,5 +109,5 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
     order = toposort(adj)
     for x in order:
-        print(x + 1, end=' ')
+        print(x + 1)
 
